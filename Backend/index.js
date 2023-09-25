e010c325dfe0e38c177fc1a2a7369ff5bc997769
Sp9751 <s96112516@gmail.com>
2023-09-25 14:43:55 +0530

    Complete Project

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const connectToMongo = require('./db')
+
+
+connectToMongo();
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+app.listen(port, ()=>{
+    console.log(`Example app listening at https://localhost:${port}`)
+})
+
